ae3c86555cdd53d1cfc695316c405c95d8c6079a
Damian de Vega <damiandevega@gmail.com>
2018-12-15 16:02:39 -0500

    moved select up to main component to share component state

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,23 +1,109 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+// import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+// import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+// import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-import Select from '../Utils/Select';
+import test_data from '../../test_data/test_data.json';
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    formControl: {
+      margin: '20px',
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing.unit * 2,
+    },
+});
 
 class Main extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            line: null
+            line: '',
+            lines: null,
+            name: '',
+            labelWidth: 0
         }
     }
 
+    componentDidMount() {
+    
+        // This is where xhr would be made to api for data
+        // Instead using test_data object from test_data.json
+        
+        this.setState({
+            lines: test_data.lines,
+            labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
+        });
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+
+
     render() {
+        const { classes } = this.props;
+
+        let options;
+
+        this.state.lines !== null ? options = (
+            this.state.lines.map(line => (
+                <MenuItem key={line.id} id={line.id} value={line.name}>{line.name}</MenuItem>
+            ))
+        ) : options = null;
+
         return (
             <div>
-                <Select />
+                <form className={classes.root} autoComplete="off">
+                    <FormControl variant="outlined" className={classes.formControl}>
+                    <InputLabel
+                        ref={ref => {
+                        this.InputLabelRef = ref;
+                        }}
+                        htmlFor="outlined-line-simple"
+                    >
+                        Select Line
+                    </InputLabel>
+                    <Select
+                        value={this.state.line}
+                        onChange={this.handleChange}
+                        input={
+                        <OutlinedInput
+                            labelWidth={this.state.labelWidth}
+                            name="line"
+                            id="outlined-line-simple"
+                        />
+                        }
+                    >
+                        <MenuItem value="">
+                        <em>None</em>
+                        </MenuItem>
+                        {options}
+                    </Select>
+                    </FormControl>
+                </form>
             </div>
         )
     }
 }
 
-export default Main;
+Main.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Main);
