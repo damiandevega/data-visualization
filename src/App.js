f59ce311e6123407d81b7f1939c12816eb7499dc
Damian de Vega <damiandevega@gmail.com>
2018-12-15 14:27:06 -0500

    organizing the component structure before moving forward

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 import Header from './components/Header';
 
+import Divider from '@material-ui/core/Divider';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Header title="Line Search" />
+        <Divider/>
       </div>
     );
   }
